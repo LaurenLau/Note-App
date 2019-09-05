fd0a3847d28b72cdbfc6d79adaedd9e5e36de808
Laurenlau <harris_lau@hotmail.co.uk>
2019-09-05 12:18:07 +0100

    Refactoring note controller

--- src/noteController.js
+++ src/noteController.js
@@ -12,14 +12,13 @@
     this.list.add('Hi')
     this.list.add('Hello')
     this.list.add('Hey')
-
+    
     var view = new NoteListView(this.list)
 
     document.getElementById("app").innerHTML = view.convert()
   };
-
-
-
   exports.NoteController = NoteController;
 })(this);
 
+var ctrl = new NoteController()
+ctrl.control()
